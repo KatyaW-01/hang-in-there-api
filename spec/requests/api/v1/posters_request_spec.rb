e446b3214e52b7d688b68cc31bcad2f357917333
Katya Weicht <katya.weicht@gmail.com>
2025-04-08 21:37:28 -0600

    Write test for creating a new poster

--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -58,4 +58,27 @@ describe "Posters API", type: :request do
       expect(attributes[:img_url]).to be_a(String) 
     end
   end
+  it 'can create a new poster' do
+    poster_params = {
+                        name: "HOPELESSNESS",
+                        description: "Stay in your comfort zone; it's safer.",
+                        price: 112.00,
+                        year: 2020,
+                        vintage: true,
+                        img_url: "./assets/hopelessness.jpg"
+    }
+    headers = { "CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/posters", headers: headers, params: JSON.generate(poster: poster_params)
+    created_poster = Poster.last
+
+    expect(response).to be_successful
+    expect(created_poster.name).to eq(poster_params[:name])
+    expect(created_poster.description).to eq(poster_params[:description])
+    expect(created_poster.price).to eq(poster_params[:price])
+    expect(created_poster.year).to eq(poster_params[:year])
+    expect(created_poster.vintage).to eq(poster_params[:vintage])
+    expect(created_poster.img_url).to eq(poster_params[:img_url])
+  end
 end
+
