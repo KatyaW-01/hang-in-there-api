d6c6bbfeec8991d2e1c98ad928edf57f873214f9
Katya Weicht <katya.weicht@gmail.com>
2025-04-12 09:09:46 -0600

    Fix spacing in controller

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -17,10 +17,12 @@ class Api::V1::PostersController < ApplicationController
       render json: PosterSerializer.format_posters(posters)
     end
   end
+
   def show
     poster = Poster.find(params[:id])
     render json: PosterSerializer.format_poster(poster)
   end
+
   def create
     poster = Poster.create(poster_params)
     render json: PosterSerializer.format_poster(poster)
@@ -36,7 +38,9 @@ class Api::V1::PostersController < ApplicationController
   end
 
   private
+  
     def poster_params
       params.require(:poster).permit(:name,:description,:price,:year,:vintage,:img_url)
     end
+    
 end
